a647d339a9c96f4cd2ca67ea49d936c3e298d49e
1852254-cmd <1852254@fcpsschools.net>
2025-10-07 14:35:49 +0000

    added java file

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    // Program logic goes here
+        int num = 0;
+        System.out.println("Hello, World! " + num);
+    }
+}
